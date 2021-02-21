1e8605592a698a5e9f185faa88c88393133b41d4
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-22 02:47:20 +0530

    [sumit] game over

--- TicTacToe.java
+++ TicTacToe.java
@@ -198,5 +198,6 @@ public class TicTacToe {
 		int chance = game.firstChance();
 		game.gamePlay(player, computer, board, chance, game);
 		game.showBoard(board);
+		System.out.println("******THANK YOU! VISIT AGAIN******");
 	}
 }
