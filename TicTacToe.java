a7b0f13f7d6445eea6963782bf4d985306a8c18a
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-22 01:47:16 +0530

    [sumit] give location player

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class TicTacToe {
-	public void gameBoard() {
+	public char[] gameBoard() {
 		char[] board = new char[10];
 		for(int i = 1; i < board.length; i++) {
 			board[i] = ' ';
-			System.out.println(board[i]);
+			
 		}
+		return board;
 	}
-	public void playerSelection() {
+	public char playerSelection() {
 		char player, comp;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter x for chhosing cross and enter o for choosing circle");
@@ -20,20 +22,35 @@ public class TicTacToe {
 			player = 'o';
 			comp = 'x';
 		}
-		System.out.println("Player is " + player + " and Computer is " + comp);
+		return player;
 	}
-	public void showBoard() {
-		System.out.println("  |  |  ");
-		System.out.println("--------");
-		System.out.println("  |  |  ");
-		System.out.println("--------");
-		System.out.println("  |  |  ");
+	public void showBoard(char[] board) {
+		System.out.println(board[1] + "  | " + board[2] + " | " + board[3]);
+		System.out.println("--------------");
+		System.out.println(board[4] + "  | " + board[5] + " | " + board[6]);
+		System.out.println("--------------");
+		System.out.println(board[7] + "  | " + board[8] + " | " + board[9]);
+	}
+	public void userInput(char[] board, char player) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number from 1 to 9");
+		int user_input = input.nextInt();
+		if (user_input < 10 && user_input > 0) {
+			if (board[user_input] == ' ') {
+				System.out.println("Place is available");
+			}
+			else
+				System.out.println("Place is already taken");
+		}
+		else
+			System.out.println("Invalid Input");
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to tictactoe game");
 		TicTacToe game = new TicTacToe();
-		game.gameBoard();
-		game.playerSelection();
-		game.showBoard();
+		char[] board = game.gameBoard();
+		char player = game.playerSelection();
+		game.showBoard(board);
+		game.userInput(board, player);
 	}
 }
